b4ad3a6e59211192f0b26a0a16644cc85d92b4b5
FadillRvlog24 <fadillahadit346@email.com>
2023-12-05 08:36:58 +0700

    menu awal program

---
+++ menu awal program.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	char pilihan;
+	
+    std::cout << "=====================================\n";
+    std::cout << "   Pengembangan Aplikasi CRUD Dosen  \n";
+    std::cout << "=====================================\n";
+
+    
+        std::cout << "\nMenu:\n";
+        std::cout << "1. Tambah Data Dosen\n";
+        std::cout << "2. Lihat Data Dosen\n";
+        std::cout << "3. Update Data Dosen\n";
+        std::cout << "4. Hapus Data Dosen\n";
+        std::cout << "5. Keluar\n";
+        std::cout << "Masukkan Pilihan : ";
+        std::cin >> pilihan;
+}
